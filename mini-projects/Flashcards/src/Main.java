9f07c4b6d64801336dd33c06e5bde86079acc005
mehmetkirlak <kirlakmehmet@outlook.com>
2022-12-04 23:30:05 +0300

    adjustment

--- mini-projects/Flashcards/src/Main.java
+++ mini-projects/Flashcards/src/Main.java
@@ -1,165 +1,285 @@
-import java.io.*;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final Map<String, String> map = new HashMap<>();
-
-    public static void main(String[] args) throws IOException {
-//        map.put("mehmet","kirlak");
-//        map.put("fazullah","demir");
-//        map.put("garip","asalak");
-//        map.put("kerem","gobi");
-
-        start();
-    }
-
-    public static void start() throws IOException {
-
-
-        boolean flag = false;
-        while(!flag){
-            System.out.println("Input the action (add, remove, import, export, ask, exit):");
-            String action = br.readLine();
-            switch (action){
-                case "add":
-                    add();
-                    break;
-                case "remove":
-                    remove();
-                    break;
-                case  "exit":
-                    flag = exit();
-                    break;
-                case "import":
-                    importFile();
-                    break;
-                case "export":
-                    exportFile();
-                    break;
-                case "ask":
-                    ask();
-                    break;
+    public static void main(String[] args) {
+        FlashCards fc = new FlashCards();
+        boolean saveOnExit = false;
+        String saveOnExitFileName = "";
+        if (args.length>0){ // import mehmet.txt export ahmet.txt import veli.txt export dayı.txt
+            for (int i = 0 ; i < args.length ; i+=2) {
+                if (args[i] == "-import") {
+                    fc.importCardsAsMainArgs(args[i+1]);
+                } else {
+                    saveOnExitFileName = args[i+1];
+                    saveOnExit = true;
+                }
             }
         }
+        fc.mainMenu();
+        if (saveOnExit){
+            fc.exportCardsAsMainArgs(saveOnExitFileName);
+        }
+    }
+}
 
+class FlashCards {
+
+    private final Scanner scanner = new Scanner(System.in);
+    private final Map<String, String> flashCards = new HashMap<>();
+    private final Map<String, Integer> cardStats = new HashMap<>();
+    private final StringBuilder log = new StringBuilder();
+
+    public void mainMenu() {
+        boolean flag = true;
+        while (flag) {
+            printlnLog("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):");
+            switch (scNextLineLog()) {
+                case "add": {
+                    addCard();
+                    break;
+                }
+                case "remove": {
+                    removeCard();
+                    break;
+                }
+                case "import": {
+                    importCards();
+                    break;
+                }
+                case "export": {
+                    exportCards();
+                    break;
+                }
+                case "ask": {
+                    askCards();
+                    break;
+                }
+                case "log": {
+                    saveLog();
+                    break;
+                }
+                case "hardest card": {
+                    hardestStats();
+                    break;
+                }
+                case "reset stats": {
+                    resetStats();
+                    break;
+                }
+                case "exit": {
+                    printlnLog("Bye bye!");
+                    flag = false;
+                }
+            }
+        }
     }
 
-    public static void add() throws IOException {
-        System.out.println("The card:");
-        String term = br.readLine();
-        if (!map.containsKey(term)) {
-            map.put(term, null);
-        } else {
-            System.out.printf("The card \"%s\" already exists.\n\n", term);
+    private void addCard() {
+        printlnLog("The card:");
+        String term = scNextLineLog();
+        if (dupPresent(term, 0)) {
+            printlnLog("The card \"" + term +"\" already exists.\n");
             return;
         }
-        System.out.printf("The definition of the card:\n");
-        String definition = br.readLine();
-        if (!map.containsValue(definition)) {
-            map.put(term, definition);
-            System.out.printf("The pair (\"%s\":\"%s\") has been added.\n\n", term, definition);
+        printlnLog("The definition of the card:");
+        String def = scNextLineLog();
+        if (dupPresent(def, 1)) {
+            printlnLog("The definition \"" + def + "\" already exists.\n");
         } else {
-            System.out.printf("The definition \"%s\" already exists.\n\n", definition);
+            flashCards.put(term, def);
+            cardStats.put(term, 0);
+            printlnLog("The pair (\"" + term + "\":\"" + def + "\") has been added.\n");
         }
     }
 
-    public static void remove() throws IOException {
-        System.out.println("Which card?");
-        String cardName = br.readLine();
-        if (map.containsKey(cardName)){
-            map.remove(cardName);
-            System.out.println("The card has been removed.\n");
+    private void removeCard() {
+        printlnLog("Which card?");
+        String s = scNextLineLog();
+        if (flashCards.containsKey(s)) {
+            flashCards.remove(s);
+            cardStats.remove(s);
+            printlnLog("The card has been removed.\n");
         } else {
-            System.out.println("Can't remove \""+cardName+"\": there is no such card.\n");
+            printlnLog("Can't remove \"" + s + "\": there is no such card.\n");
         }
     }
 
-    public static void ask() throws IOException {
-        System.out.println("How many times to ask?");
-        int input = Integer.parseInt(br.readLine());
-        int counter = 0;
-        Map<String, String> tempMap = map;
+    private void importCards() {
+        printlnLog("File name:");
+        File file = new File(scNextLineLog());
+        int count = 0;
+        String[] s;
+        if (file.exists()) {
+            try (Scanner fileS = new Scanner(file)) {
+                while (fileS.hasNext()) {
+                    s = fileS.nextLine().split("=");
+                    flashCards.put(s[0], s[1]);
+                    cardStats.put(s[0], Integer.parseInt(s[2]));
+                    count++;
+                }
+                printlnLog(count + " cards have been loaded.\n");
+            } catch (IOException ex) {
+                printlnLog("Problem with a file. " + ex.getMessage() + ".\n");
+            }
+        } else {
+            printlnLog("File not found.\n");
+        }
+    }
 
-        for (var entry : tempMap.entrySet()) {
-            if (counter < input) {
-                System.out.print("Print the definition of \"" + entry.getKey() + "\":");
-                String answer = br.readLine();
-                if (answer.equals(entry.getValue())) {
-                    System.out.printf("Correct!\n");
-                } else {
-                    if (map.containsValue(answer)) {
-                        String correctAnswer = "";
-                        for (var tempEntry: tempMap.entrySet()) {
-                            if (tempEntry.getValue().equals(answer)) {
-                                correctAnswer = tempEntry.getKey();
-                                break;
-                            }
+    public void importCardsAsMainArgs(String fileName) {
+        File file = new File(fileName);
+        int count = 0;
+        String[] s;
+        if (file.exists()) {
+            try (Scanner fileS = new Scanner(file)) {
+                while (fileS.hasNext()) {
+                    s = fileS.nextLine().split("=");
+                    flashCards.put(s[0], s[1]);
+                    cardStats.put(s[0], Integer.parseInt(s[2]));
+                    count++;
+                }
+                printlnLog(count + " cards have been loaded.\n");
+            } catch (IOException ex) {
+                printlnLog("Problem with a file. " + ex.getMessage() + ".\n");
+            }
+        } else {
+            printlnLog("File not found.\n");
+        }
+    }
+
+    private void exportCards() {
+        printlnLog("File name:");
+        File file = new File(scNextLineLog());
+        try (FileWriter fw = new FileWriter(file)) {
+            for (var entry : flashCards.entrySet()) {
+                fw.write(entry.getKey() + "=" + entry.getValue() + "=" + cardStats.get(entry.getKey()) + "\n");
+            }
+            printlnLog(flashCards.size() + " cards have been saved.\n");
+        } catch (IOException ex) {
+            printlnLog("Problem with a file. " + ex.getMessage() + ".\n");
+        }
+    }
+
+    public void exportCardsAsMainArgs(String fileName) {
+        File file = new File(fileName);
+        try (FileWriter fw = new FileWriter(file)) {
+            for (var entry : flashCards.entrySet()) {
+                fw.write(entry.getKey() + "=" + entry.getValue() + "=" + cardStats.get(entry.getKey()) + "\n");
+            }
+            printlnLog(flashCards.size() + " cards have been saved.\n");
+        } catch (IOException ex) {
+            printlnLog("Problem with a file. " + ex.getMessage() + ".\n");
+        }
+    }
+
+    private void askCards() {
+        printlnLog("How many times to ask?");
+        int t = Integer.parseInt(scNextLineLog()) ;
+        boolean correct = false;
+        for (var entry : flashCards.entrySet()) {
+            if (t < 1) { break; }
+            printlnLog("Print the definition of \"" + entry.getKey() + "\":");
+            String def = scNextLineLog();
+            if (entry.getValue().equals(def)) {
+                printlnLog("Correct!");
+                correct = true;
+            }
+            if (!correct) {
+                printLog("Wrong. The right answer is \"" + entry.getValue() + "\"");
+                if (flashCards.containsValue(def)) {
+                    for (String key : flashCards.keySet()) {
+                        if (flashCards.get(key).equals(def)) {
+                            printlnLog(", but your definition is correct for \"" + key +"\".\n");
                         }
-                        System.out.printf(
-                                "Wrong. The right answer is \"%s\", " +
-                                        "but your definition is correct for \"%s\".\n\n", entry.getValue(), correctAnswer
-                        );
-                    } else {
-                        System.out.printf("Wrong. The right answer is \"%s\".\n\n", entry.getValue());
                     }
+                } else {
+                    printlnLog(".\n");
                 }
-            } else {
-                break;
+                cardStats.put(entry.getKey(), cardStats.get(entry.getKey()) + 1);
             }
+            correct = false;
+            t--;
         }
     }
 
-    public static void exportFile() throws IOException {
-        System.out.println("File name:");
-        Map<String, String> tempMap = map;
-        String input = br.readLine();
-        File file = new File("./" + input);
-        try(FileWriter writer = new FileWriter(file, false)) {
-            for (var entry : tempMap.entrySet()) {
-                writer.write(entry.getKey() + ":" + entry.getValue() + "\n");
-            }
-            System.out.printf("%d cards have been saved.\n\n", tempMap.size());
-        } catch (IOException e) {
-            System.out.printf("An exception occurred %s\n\n", e.getMessage());
+    //type 0 - term, type 1 - definition
+    private boolean dupPresent(String s, int type) {
+        if (type == 0 && flashCards.containsKey(s)) {
+            return true;
+        }
+        if (type == 1 && flashCards.containsValue(s)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void saveLog() {
+        printlnLog("File name:");
+        File file = new File(scNextLineLog());
+        try (FileWriter fw = new FileWriter(file)) {
+            fw.write(log.toString());
+            printlnLog("The log has been saved.\n");
+        } catch (IOException ex) {
+            printlnLog("Problem with a file. " + ex.getMessage() + ".\n");
         }
     }
-    public static void importFile() throws IOException {
-        System.out.println("File name:");
-        String input = br.readLine();
-        int lineCounter = 0;
-        File file = new File("./" + input);
-        try {
-            Scanner myReader = new Scanner(file);
-            while(myReader.hasNextLine()) {
-                String[] data = myReader.nextLine().split(":");
-                if (map.containsKey(data[0])) {
-                    map.replace(data[0], data[1]);
+
+    private void hardestStats() {
+        List<String> maxKeys = new ArrayList<>();
+        int max = 0;
+        for (var entry : cardStats.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+            }
+        }
+        if (max == 0) {
+            printlnLog("There are no cards with errors.\n");
+            return;
+        }
+        for (var entry : cardStats.entrySet()) {
+            if (entry.getValue() == max) {
+                maxKeys.add(entry.getKey());
+            }
+        }
+        if (maxKeys.size() == 1) {
+            printlnLog("The hardest card is \"" + maxKeys.get(0)  + "\". You have " + max + " errors answering it.\n");
+        } else {
+            printLog("The hardest cards are ");
+            for (int i = 0; i < maxKeys.size(); i++) {
+                if (i == maxKeys.size() - 1) {
+                    printLog("\"" + maxKeys.get(i) + "\".\n");
+                    break;
                 }
-                map.put(data[0], data[1]);
-                lineCounter++;
+                printLog("\"" + maxKeys.get(i) + "\", ");
             }
-            System.out.println(lineCounter + " cards have been loaded.\n");
-        } catch(FileNotFoundException e) {
-            System.out.println("File not found\n");
+            printLog("You have" + max + "errors answering them.\n");
         }
     }
 
-    public static boolean exit(){
-        System.out.println("Bye bye!");
-        return true;
+    private void resetStats() {
+        for (String key : cardStats.keySet()) {
+            cardStats.replace(key, 0);
+        }
+        printlnLog("Card statistics have been reset.\n");
     }
 
-    public static  <K, V> K getKey(Map<K, V> map, V value) {
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            if (entry.getValue().equals(value)) {
-                return entry.getKey();
-            }
-        }
-        return null;
+    private void printLog(String s) {
+        log.append(s);
+        System.out.print(s);
     }
+
+    private void printlnLog(String s) {
+        log.append(s).append("\n");
+        System.out.println(s);
+    }
+
+    private String scNextLineLog() {
+        String s = scanner.nextLine();
+        log.append(s).append("\n");
+        return s;
+    }
+
 }
